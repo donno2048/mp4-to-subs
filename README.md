13a71885cda475184b7b4f6d696563d640fd652b
donno2048 <just4now666666@gmail.com>
2022-03-24 18:03:08 +0200

    dither

--- README.md
+++ README.md
@@ -1,28 +1,19 @@
 # mp4-to-subs
 
-Turn mp4 input into subtitles compatible for youtube.
+Turn mp4 input into braille subtitles compatible for youtube.
 
 There is also a unicode-art version (not captions) with high resolution [here](https://github.com/donno2048/mp4-to-unicode)
 
 ## Demo
 
-https://www.youtube.com/watch?v=o9eNY1_9mis
-
-There is also a unicode-art version (not captions) with high resolution [here](https://github.com/donno2048/mp4-to-unicode)
-
-
 ## Usage
 
-Run `python3 main.py > badapple.srt`
+Run `python3 main.py badapple.srt`
 
 (Use another file with the name _video.mp4_ to convert it, you can use another output name too (other then _badapple_))
 
 ## Install dependencies
 
-`pip3 install opencv-python Pillow`
+`pip3 install opencv-python Pillow numpy`
 
 (on Linux you may need to run `sudo apt-get install ffmpeg libsm6 libxext6 -y` to use `opencv`)
-
-There is also [a branch compatible with youtube](https://github.com/donno2048/mp4-to-subs/tree/youtube) and a [demo](https://www.youtube.com/watch?v=o9eNY1_9mis)
-
-There is also [a branch compatible with vtt for youtube](https://github.com/donno2048/mp4-to-subs/tree/vtt) with higher resolution and a [demo](https://www.youtube.com/watch?v=V6jGTFwRf00)
--- main.py
+++ main.py
@@ -1,35 +1,43 @@
-try: from PIL.Image import open
-except: from Image import open
-from sys import stdout
+from PIL.Image import open as imopen
+from sys import argv
 from datetime import timedelta
 from cv2 import VideoCapture, imencode, resize
 from io import BytesIO
+from numpy import array
+SPACES = False # wheter to use spaces or not
+file = open(argv[1], "w", encoding='utf8')
 def I2T(File):
-	im = open(File)
-	(w, h) = im.size
-	mim = im.convert("1")
-	data = list(mim.getdata())
-	counter = 0
-	field = True
-	for pixel in data:
-		if field:
-			if pixel > 127: stdout.write("⬛")
-			else: stdout.write("⬜")
-		counter = counter + 1
-		if counter >= w:
-			counter = 0
-			if field: stdout.write("\n")
-			field = not field
+	im = imopen(File)
+	w, h = im.size
+	data = list(im.getdata())
+	crop = lambda x: [255 if i > 255 else 0 if i < 0 else i for i in x]
+	for y in range(h):
+		for x in range(w):
+			oldpixel = array(data[x + y * w])
+			newpixel = array(list(map(lambda i: 255 if i > 127 else 0, oldpixel)))
+			data[x + y * w] = newpixel
+			quant_error = oldpixel - newpixel
+			if x + 1 < w: data[x + 1 + y * w] = crop(data[x + 1 + y * w] + 7 * quant_error / 16)
+			if x - 1 >= 0 and y + 1 < h: data[x - 1 + (y + 1) * w] = crop(data[x - 1 + (y + 1) * w] + 3 * quant_error / 16)
+			if y + 1 < h: data[x + (y + 1) * w] = crop(data[x + (y + 1) * w] + 5 * quant_error / 16)
+			if x + 1 < w and y + 1 < h: data[x + 1 + (y + 1) * w] = crop(data[x + 1 + (y + 1) * w] + 1 * quant_error / 16)
+	black_and_white = []
+	for y in range(h):
+		for x in range(w):
+			black_and_white.append(0.3 * data[x + y * w][0] + 0.586 * data[x + y * w][1] + 0.114 * data[x + y * w][2] < 127)
+	for y in range(h // 3):
+		for x in range(w // 2):
+			file.write(chr(0x2800 + max(sum(map(lambda i: sum(map(lambda j: black_and_white[w * (i + 3 * y) + 2 * x + j], [0, 1])), [0, 1, 2])), not SPACES)))
+		file.write("\n")
 vidcap = VideoCapture('./video.mp4')
 success, image = vidcap.read()
 index = 0
 while success:
 	index += 1
-	print(index)
-	print((str(timedelta(milliseconds = index * 100))[:11] + " --> " + str(timedelta(milliseconds = (index + 1) * 100))[:11]).replace('.', ','))
-	I2T(BytesIO(imencode(".jpg", resize(image, (24, 32), interpolation = 3))[1]))
-	print()
+	file.write(str(index) + "\n" + (str(timedelta(milliseconds = index * 100))[:11] + " --> " + str(timedelta(milliseconds = (index + 1) * 100))[:11]).replace('.', ',') + "\n")
+	I2T(BytesIO(imencode(".jpg", resize(image, (42, 42), interpolation = 3))[1]))
+	file.write("\n")
 	vidcap.read()
 	vidcap.read()
 	success, image = vidcap.read()
-	
+file.close()
